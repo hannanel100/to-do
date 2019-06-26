ba7183d40c827b5b74462d67f67fa97a0b25d819
hannanel gershinsky <hannanel100@gmail.com>
2019-06-26 21:26:53 +0300

    still need to work on local storage

--- to-do.js
+++ to-do.js
@@ -6,12 +6,16 @@ let reset = document.getElementById("reset");
 let content = document.getElementById("content");
 let noteDiv = '';
 let noteArray = [];
-let noteObj = {
+let xIcon = '';
+let noteObjTemplate = {
+    id: "some",
     text: "some",
     date: "some",
     time: "some"
 };
-let appNodes = [];
+let objOfArray = {
+    arr: []
+}
 let ctr = 0;
 add.addEventListener("click", function (e) {
     e.preventDefault();
@@ -21,28 +25,46 @@ add.addEventListener("click", function (e) {
     toDoDate.value = '';
     let timeValue = toDoTime.value;
     toDoTime.value = '';
-    buildPage(textValue, dateValue, timeValue);
+    let noteObj = Object.create(noteObjTemplate);
+    let tempId;
+    tempId = buildPage(textValue, dateValue, timeValue);
+    noteObj.id = tempId; 
     noteObj.text = textValue;
     noteObj.date = dateValue;
     noteObj.time = timeValue;
-    noteArray.push(Object.create(noteObj));
-    //JSON.stringify(noteArray)
-    localStorage.setItem("appNodes[" + ctr + "]", JSON.stringify(noteObj));
-    ctr++;
-    
-    
+    noteArray.push(noteObj);
+    //objOfArray.arr = JSON.stringify(noteArray);
+    localStorage.setItem("noteArray", JSON.stringify(noteArray));
+    objOfArray = localStorage.getItem("noteArray");
+    console.log("objOfArray after parse: "+ JSON.parse(objOfArray));
+    console.log(objOfArray[0].id)
+    //still need to work on local storage
+
+    noteDiv.addEventListener("mouseenter", function(){
+        this.childNodes[0].style.opacity = 1;
+        xIcon.addEventListener("click",function(){
+            
+            //delete from local storage and reload page
+        });
+    })
+    noteDiv.addEventListener("mouseleave", function(){
+        if(this.childNodes[0].style.opacity == 1){
+            this.childNodes[0].style.opacity = 0;
+        }
+    });
 });
 
-let xIcon = document.createElement('i');
+
 function buildPage(textValue, dateValue, timeValue) {
-
-
+    let noteDivId = "note"+ctr;
     noteDiv = document.createElement('div');
     noteDiv.setAttribute('class', 'note hide');
+    noteDiv.setAttribute('id', noteDivId);
     let textDiv = buildDiv("text-cl", "text");
     let dateDiv = buildDiv("text-cl", "date");
     let timeDiv = buildDiv("text-cl", "time");
-    xIcon.setAttribute('class', 'fas');
+    xIcon = document.createElement('i');
+    xIcon.setAttribute('class', 'fas fa-times');
     textDiv.innerHTML = textValue;
     dateDiv.innerHTML = dateValue;
     timeDiv.innerHTML = timeValue;
@@ -50,14 +72,14 @@ function buildPage(textValue, dateValue, timeValue) {
     noteDiv.appendChild(textDiv);
     noteDiv.appendChild(dateDiv);
     noteDiv.appendChild(timeDiv);
-    
-
     content.insertBefore(noteDiv, content.childNodes[0]);
     noteDiv.classList.add('show');
     noteDiv.classList.remove('hide');
-    
-
+    ctr++;
+    console.log("noteDiv id: "+noteDivId);
+    return noteDivId;
 }
+
 function buildDiv(cl1, cl2) {
     var divElement = document.createElement('div');
     divElement.setAttribute('class', cl1 + " " + cl2);
