aa77bd016a3276e3c708fe2ee6c796145b138f5f
hannanel gershinsky <hannanel100@gmail.com>
2019-06-30 21:46:16 +0300

    ichs

--- to-do.js
+++ to-do.js
@@ -6,77 +6,99 @@
     let reset = document.getElementById("reset");
     let content = document.getElementById("content");
     let noteDiv = '';
-    let noteArray =[];
+    let noteArray = JSON.parse(localStorage.getItem("noteArray"));
     let xIcon = '';
-    let lastId;
+    let lastId = localStorage.getItem("lastId");
+    let ctr;
+    let rebuild = true;
     let noteObjTemplate = {
-        id: "note"+lastId,
+        id: "some",
         text: "some",
         date: "some",
         time: "some"
     };
-    let arrOfObj = {
-        arr: [""]
-    };
-    if (localStorage.length > 1) {
+
+    if (noteArray && noteArray.length > 0) {
         noteArray = (JSON.parse(localStorage.getItem("noteArray")));
-        console.log()
-        lastId = "note" + (noteArray.length);//gets the last id
+        //console.log("if noteArray.legth>0" + noteArray.length);
+        ctr = Number(lastId.match(/\d+/g).map(Number)) + 1;
+        for (let i = 0; i < noteArray.length; i++) {
+            buildPage(noteArray[i]);
+            noteDiv.addEventListener("mouseenter", function (e) {
+                this.childNodes[0].style.opacity = 1;
+        
+            })
+            noteDiv.addEventListener("mouseleave", function (e) {
+                if (this.childNodes[0].style.opacity == 1) {
+                    this.childNodes[0].style.opacity = 0;
+                }
+            });
+        }
     }
     else {
+        rebuild = false;
         noteArray = [];
-        lastId = "note0";
+        ctr = 0;
     }
 
 
     add.addEventListener("click", function (e) {
+
         e.preventDefault();
         let noteObj = createObj(toDoText.value, toDoDate.value, toDoTime.value);
         buildPage(noteObj);
         noteArray.push(noteObj);
-        localStorage.setItem("noteArray", JSON.stringify(noteArray));
-        //get the array of objects. 
-        noteArray = (JSON.parse(localStorage.getItem("noteArray")));
-        //console.log("arrOfObj[0].text: " + JSON.stringify(arrOfObj[0].text));
-        // if (noteArray.length > 0) {
-        //     lastId = "note" + (noteArray.length);//gets the last id
-        //     debugger;
-        // }
-        // else {
-        //     lastId = "note0";
-        // }
-
-        localStorage.setItem("lastId", lastId);//saves last id to localStorage. still gets overwritten when reload page
-        noteDiv.addEventListener("mouseenter", function () {
+        //console.log(noteArray);
+        reloadArr();
+        noteDiv.addEventListener("mouseenter", function (e) {
             this.childNodes[0].style.opacity = 1;
-            xIcon.addEventListener("click", function () {
-                //delete from local storage and reload page
-            });
+    
         })
-        noteDiv.addEventListener("mouseleave", function () {
+        noteDiv.addEventListener("mouseleave", function (e) {
             if (this.childNodes[0].style.opacity == 1) {
                 this.childNodes[0].style.opacity = 0;
             }
         });
         return clear();
     });
+    
 
+
+    function reloadArr() {
+        localStorage.setItem("noteArray", JSON.stringify(noteArray));
+        //get the array of objects. 
+        noteArray = (JSON.parse(localStorage.getItem("noteArray")));
+        localStorage.setItem("lastId", lastId);//saves last id to localSt orage. still gets overwritten when reload page
+        lastId = localStorage.getItem("lastId");
+    }
     function createObj(textValue, dateValue, timeValue) {
         let noteObj = Object.create(noteObjTemplate);
-        noteObj.id = lastId;
+
         noteObj.text = textValue;
         noteObj.date = dateValue;
         noteObj.time = timeValue;
         return noteObj;
     }
-    function buildPage(noteObj) {
+    function updatePage(noteObj) {
         noteDiv = document.createElement('div');
         noteDiv.setAttribute('class', 'note hide');
-        noteDiv.setAttribute('id', lastId);
+        noteDiv.setAttribute('id', noteObj.id);
         buildNote(noteDiv, noteObj);
-        //ctr++;
-        //console.log("noteDiv id: " + noteDivId);
-        //return noteDivId;
+    }
+    function buildPage(noteObj) {
+        if(rebuild === true){
+            updatePage(noteObj);
+        }
+        else{
+            noteDiv = document.createElement('div');
+            noteDiv.setAttribute('class', 'note hide');
+            lastId = "note" + ctr;
+            noteDiv.setAttribute('id', lastId);
+            noteObj.id = lastId;
+            buildNote(noteDiv, noteObj);
+            ctr++;
+        }
+        
     }
 
     function buildDiv(cl1, cl2) {
