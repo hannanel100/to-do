9947378231bbeb06a78381312f50ac789e2ac6bb
Hannanel Gershinsky <hannanel100@gmail.com>
2019-07-09 23:38:43 +0300

    done!

--- to-do.js
+++ to-do.js
@@ -1,130 +1,127 @@
 (function () {
-    let toDoText = document.getElementById("to-do-text");
-    let toDoDate = document.getElementById("to-do-date");
-    let toDoTime = document.getElementById("to-do-time");
-    let add = document.getElementById("add");
-    let reset = document.getElementById("reset");
-    let content = document.getElementById("content");
-    let noteDiv = '';
-    let noteArray = JSON.parse(localStorage.getItem("noteArray"));
-    let xIcon = '';
-    let lastId = localStorage.getItem("lastId");
-    let ctr;
-    let rebuild = true;
+    let noteTemplate = `<div class="note hide" id={{id}}>
+    <i class="fas fa-times"></i>
+    <div class="text text-cl">{{text}}</div>
+    <div class="date text-cl">{{date}}</div>
+    <div class="time text-cl">{{time}}</div>
+    </div>`;
+    const form = document.querySelector("form");
+    const toDoText = document.getElementById("to-do-text");
+    const toDoDate = document.getElementById("to-do-date");
+    const toDoTime = document.getElementById("to-do-time");
+    const content = document.getElementById("content");
+    const reset = document.getElementById("reset");
+    let lastId;
+    let id = -1;
+    let noteArray = [];
+    let noteDiv;
     let noteObjTemplate = {
         id: "some",
         text: "some",
         date: "some",
         time: "some"
     };
+    let data = JSON.parse(localStorage.getItem('noteArray'));
+    //checking if page is refreshed
+    if (data != null) {
+        noteArray = data;
+        buildPage(noteArray);//rebuilding notes from local storage
+        lastId = noteArray[0].id;
+        delIcon();//enabling deletion icon
+    }
+    //accepting user data
+    form.addEventListener('submit', function (e) {
+        e.preventDefault();
+        let noteObj = createObj(idNum(), toDoText.value, toDoDate.value, toDoTime.value);//function returns object of note - id+user input
+        noteArray.unshift(noteObj);
+        localStorage.setItem("noteArray", JSON.stringify(noteArray));
+        getNoteTemplate(noteObj, noteTemplate);//applying noteObj to template and placing on screen
+        localStorage.setItem("lastId", noteObj.id);
+        delIcon();//enabling deletion icon
+        data = JSON.parse(localStorage.getItem('noteArray'));
+        clearInputs();//clearing input fields
+    });
 
-    if (noteArray && noteArray.length > 0) {
-        noteArray = (JSON.parse(localStorage.getItem("noteArray")));
-        //console.log("if noteArray.legth>0" + noteArray.length);
-        ctr = Number(lastId.match(/\d+/g).map(Number)) + 1;
-        for (let i = 0; i < noteArray.length; i++) {
-            buildPage(noteArray[i]);
-            noteDiv.addEventListener("mouseenter", function (e) {
-                this.childNodes[0].style.opacity = 1;
-        
-            })
-            noteDiv.addEventListener("mouseleave", function (e) {
-                if (this.childNodes[0].style.opacity == 1) {
-                    this.childNodes[0].style.opacity = 0;
+    reset.addEventListener("click", clearInputs);//enabling user to clear input fields with reset button
+    //function enabling deletion icons - visiblity and then clickability
+    function delIcon() {
+        let notes = document.getElementsByClassName("note");
+        for (let item of notes) {
+            item.addEventListener("mouseenter", function (e) {
+                let xIcon = this.childNodes[1];
+                xIcon.style.opacity = 1;//
+                xIcon.addEventListener("click", delNote);
+            });
+            item.addEventListener("mouseleave", function (e) {
+                let xIcon = this.childNodes[1];
+                if (xIcon.style.opacity == 1) {
+                    xIcon.style.opacity = 0;
                 }
             });
         }
     }
-    else {
-        rebuild = false;
-        noteArray = [];
-        ctr = 0;
-    }
-
-
-    add.addEventListener("click", function (e) {
-
-        e.preventDefault();
-        let noteObj = createObj(toDoText.value, toDoDate.value, toDoTime.value);
-        buildPage(noteObj);
-        noteArray.push(noteObj);
-        //console.log(noteArray);
-        reloadArr();
-        noteDiv.addEventListener("mouseenter", function (e) {
-            this.childNodes[0].style.opacity = 1;
-    
-        })
-        noteDiv.addEventListener("mouseleave", function (e) {
-            if (this.childNodes[0].style.opacity == 1) {
-                this.childNodes[0].style.opacity = 0;
-            }
-        });
-        return clear();
-    });
-    
-
-
-    function reloadArr() {
-        localStorage.setItem("noteArray", JSON.stringify(noteArray));
-        //get the array of objects. 
-        noteArray = (JSON.parse(localStorage.getItem("noteArray")));
-        localStorage.setItem("lastId", lastId);//saves last id to localSt orage. still gets overwritten when reload page
-        lastId = localStorage.getItem("lastId");
-    }
-    function createObj(textValue, dateValue, timeValue) {
-        let noteObj = Object.create(noteObjTemplate);
-
-        noteObj.text = textValue;
-        noteObj.date = dateValue;
-        noteObj.time = timeValue;
-        return noteObj;
-    }
-    function updatePage(noteObj) {
-        noteDiv = document.createElement('div');
-        noteDiv.setAttribute('class', 'note hide');
-        noteDiv.setAttribute('id', noteObj.id);
-        buildNote(noteDiv, noteObj);
-    }
-    function buildPage(noteObj) {
-        if(rebuild === true){
-            updatePage(noteObj);
+    //function called when clicking x icon - deletes note from screen and from local storage
+    function delNote() {
+        let delNoteId = this.parentNode.id;
+        let index = noteArray.findIndex(delId => delId.id == delNoteId);
+        this.parentNode.remove();
+        noteArray.splice(index, 1);
+        if (noteArray.length > 0) {
+            lastId = noteArray[0].id;
+            localStorage.setItem("noteArray", JSON.stringify(noteArray));
         }
-        else{
-            noteDiv = document.createElement('div');
-            noteDiv.setAttribute('class', 'note hide');
-            lastId = "note" + ctr;
-            noteDiv.setAttribute('id', lastId);
-            noteObj.id = lastId;
-            buildNote(noteDiv, noteObj);
-            ctr++;
+        //taking care of deletion of all notes
+        else {
+            lastId = null;
+            localStorage.setItem("noteArray", null);
         }
-        
     }
-
-    function buildDiv(cl1, cl2) {
-        var divElement = document.createElement('div');
-        divElement.setAttribute('class', cl1 + " " + cl2);
-        return divElement;
+    //function that deals with returning ids to notes
+    function idNum() {
+        if (lastId == null) {
+            id++;
+            return "note" + id;
+        }
+        else {
+            lastId = JSON.parse(localStorage.getItem("noteArray"))[0].id;
+            id = (Number(lastId.charAt(4)) + 1);
+            return "note" + id;
+        }
     }
-    function buildNote(noteDiv, noteObj) {
-        let textDiv = buildDiv("text-cl", "text");
-        let dateDiv = buildDiv("text-cl", "date");
-        let timeDiv = buildDiv("text-cl", "time");
-        xIcon = document.createElement('i');
-        xIcon.setAttribute('class', 'fas fa-times');
-        textDiv.innerHTML = noteObj.text;
-        dateDiv.innerHTML = noteObj.date;
-        timeDiv.innerHTML = noteObj.time;
-        noteDiv.appendChild(xIcon);
-        noteDiv.appendChild(textDiv);
-        noteDiv.appendChild(dateDiv);
-        noteDiv.appendChild(timeDiv);
+    //function that creates and returns noteObj based on id and user input
+    function createObj(id, textValue, dateValue, timeValue) {
+        let noteObjTemp = Object.create(noteObjTemplate);
+        noteObjTemp.id = id;
+        noteObjTemp.text = textValue;
+        noteObjTemp.date = dateValue;
+        noteObjTemp.time = timeValue;
+        return noteObjTemp;
+    }
+    //function that uses template and noteObj to make and return div containing note
+    function noteMaker(noteObj, template) {
+        let dummyDiv = document.createElement('div');
+        template = template.replace('{{id}}', noteObj.id);
+        template = template.replace('{{text}}', noteObj.text);
+        template = template.replace('{{date}}', noteObj.date);
+        template = template.replace('{{time}}', noteObj.time);
+        dummyDiv.innerHTML = template;
+        return dummyDiv;
+    }
+    //function that deals with building notes on page from local storage after refresh
+    function buildPage(noteArray) {
+        for (let j = noteArray.length-1; j >= 0; j--) {
+            getNoteTemplate(noteArray[j], noteTemplate);
+        }
+    }
+    //function that gets template and noteObj and calls helper function to create note and adds note to page
+    function getNoteTemplate(noteObj, noteTemplate) {
+        noteDiv = noteMaker(noteObj, noteTemplate).firstChild;
         content.insertBefore(noteDiv, content.childNodes[0]);
         noteDiv.classList.add('show');
         noteDiv.classList.remove('hide');
     }
-
-    function clear() {
+    //function to clear input fields
+    function clearInputs() {
         toDoText.value = '';
         toDoDate.value = '';
         toDoTime.value = '';
